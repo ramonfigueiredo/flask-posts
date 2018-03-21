44e776b50cfdb87277ae0a1ef54ae57753c8bf4a
Ramon Figueiredo Pessoa <ramon.fgrd@gmail.com>
2018-03-21 11:52:26 -0400

    Updating environment_variables.sh

--- environment_variables.sh
+++ environment_variables.sh
@@ -1,5 +1,3 @@
 ## LOCAL: Example to set environment variable in linux (ubuntu)
 export APP_SETTINGS="config.DevelopmentConfig"
-
-## LOCAL: Example to set environment variable in linux (ubuntu)
 export DATABASE_URL="postgresql://postgres:postgres@localhost:5432/flask_posts"
