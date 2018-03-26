2d728d415da4338188be562bbfe1be182a43768b
Ramon Figueiredo Pessoa <ramon.fgrd@gmail.com>
2018-03-26 13:20:42 -0400

    Updating db_create.py file

--- db_create.py
+++ db_create.py
@@ -1,12 +1,15 @@
 from project import db
-from project.models import BlogPost
+from project.models import User, BlogPost
 
 # create the database and db tables
 db.create_all()
 
+# insert data
+db.session.add(User("admin", "admin@mail.com", "admin"))
+
 # insert
-db.session.add(BlogPost("Good", "I\'m good."))
-db.session.add(BlogPost("Well", "I\'m well."))
+db.session.add(BlogPost("Good", "I\'m good.", 1))
+db.session.add(BlogPost("Well", "I\'m well.", 1))
 
 # commit the changes
 db.session.commit()
--- db_create_users.py
+++
@@ -1,9 +0,0 @@
-from project import db
-from project.models import User
-
-# insert data
-db.session.add(User("testA", "testA@mail.com", "passTestA"))
-db.session.add(User("testB", "testBA@mail.com", "tBpass"))
-
-# commit the changes
-db.session.commit()
