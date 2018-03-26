7f90e72317813fe77594f227aa78f60ccc1c2953
Ramon Figueiredo Pessoa <ramon.fgrd@gmail.com>
2018-03-26 10:10:49 -0400

    Fixing indentation

--- project/users/form.py
+++ project/users/form.py
@@ -9,21 +9,21 @@ class LoginForm(FlaskForm):
 
 
 class RegisterForm(FlaskForm):
-    username = TextField(
-        'username',
-        validators=[DataRequired(), Length(min=3, max=25)]
-    )
-    email = TextField(
-        'email',
-        validators=[DataRequired(), Email(message=None), Length(min=6, max=40)]
-    )
-    password = PasswordField(
-        'password',
-        validators=[DataRequired(), Length(min=6, max=25)]
-    )
-    confirm = PasswordField(
-        'Repeat password',
-        validators=[
-            DataRequired(), EqualTo('password', message='Passwords must match.')
-        ]
-    )
+	username = TextField(
+		'username',
+		validators=[DataRequired(), Length(min=3, max=25)]
+	)
+	email = TextField(
+		'email',
+		validators=[DataRequired(), Email(message=None), Length(min=6, max=40)]
+	)
+	password = PasswordField(
+		'password',
+		validators=[DataRequired(), Length(min=6, max=25)]
+	)
+	confirm = PasswordField(
+		'Repeat password',
+		validators=[
+			DataRequired(), EqualTo('password', message='Passwords must match.')
+		]
+	)
